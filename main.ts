bbddfec166c04b17998616c8bd846be3ac768563
Vadim <hawaiianwork@mail.ru>
2024-04-24 00:14:26 +0300

    upd

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 window.addEventListener("load", () => {
-  const form = document.querySelector("#new-task-form");
-  const input = document.querySelector("#new-task-input");
-  const list_el = document.querySelector("#tasks");
+  const form = document.querySelector("#new-task-form") as HTMLFormElement;
+  const input = document.querySelector("#new-task-input") as HTMLInputElement;
+  const list_el = document.querySelector("#tasks") as HTMLDivElement;
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
